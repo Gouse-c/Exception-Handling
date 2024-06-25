6e9229cc52500dbb848e887e1df52ea0d81b5c7d
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-06-25 08:49:00 +0530

    Add files via upload

---
+++ GetMessageMethod1.java
@@ -0,0 +1,14 @@
+class GetMessageMethod1
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			System.out.println(10/0);
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println(ae.getMessage());
+		}
+	}
+}
